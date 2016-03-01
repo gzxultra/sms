37af0d6f58f8c964a991ac01764d4f8b0d787e90
cadl <ctrlaltdeleteliu@gmail.com>
2016-03-01 17:34:59 +0800

    生产环境celery加入sentry错误收集

--- smsserver/tasks.py
+++ smsserver/tasks.py
@@ -9,6 +9,14 @@ celery = Celery('celery', broker=Config.CELERY_BROKER_URL)
 celery.conf.update(CELERY_ACKS_LATE=True)
 
 
+if not Config.DEBUG:
+    from raven import Client
+    from raven.contrib.celery import register_signal
+
+    client = Client(Config.CELERY_SENTRY_DSN)
+    register_signal(client)
+
+
 @celery.task
 def async_send_verification_code(sms_verification_id):
     obj = SMSVerification.get(sms_verification_id)
