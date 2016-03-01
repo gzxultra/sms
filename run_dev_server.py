936523a453b92996a36912a5dd7ecd92a7ab17c3
cadl <ctrlaltdeleteliu@gmail.com>
2016-03-01 11:41:57 +0800

    允许指定端口。默认为开发配置环境变量

--- run_dev_server.py
+++ run_dev_server.py
@@ -1,5 +1,18 @@
-# coding: utf8
+# coding: utf-8
+
+import sys
+import os
+
+if not os.environ.get('SMSSERVER_CONFIG'):
+    os.environ['SMSSERVER_CONFIG'] = 'conf.develop'
 
 from smsserver import app
 
-app.run(host='0.0.0.0', port=8002)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        port = int(sys.argv[1])
+    else:
+        port = 8001
+
+    app.run(host='0.0.0.0', port=port)
