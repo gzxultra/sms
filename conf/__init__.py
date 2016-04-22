f30f5010524f471acca7d8ef8df00d9245bb0104
Alex.F <gfreezy@gmail.com>
2016-04-22 10:44:29 +0800

    Update __init__.py

--- conf/__init__.py
+++ conf/__init__.py
@@ -1,1 +1,9 @@
+# coding: utf-8
+import os
 
+__all__ = ['Config']
+
+config_module_path = os.environ.get('SMSSERVER_CONFIG', '')
+
+config_module = __import__(config_module_path, fromlist=['Config'])
+Config = config_module.Config
