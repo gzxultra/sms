23f2614255721121e48172ec65fc181851dc604c
ushuz <hszqqq12@gmail.com>
2016-12-14 19:48:24 +0800

    Update create_sms_provider.py

--- script/create_sms_provider.py
+++ script/create_sms_provider.py
@@ -1,8 +1,10 @@
-# coding: utf8
+# coding: utf-8
 
 from smsserver.models.sms_center import SMSProvider
 from smsserver.models.const import SMSProviderIdent
 
 
 if __name__ == '__main__':
-    SMSProvider.create('云片', SMSProviderIdent.yunpian, 20)
+    SMSProvider.create(name=u'云片', ident=SMSProviderIdent.yunpian, weight=10)
+    SMSProvider.create(name=u'大汉三通', ident=SMSProviderIdent.dahansantong, weight=0)
+    SMSProvider.create(name=u'阿里大于', ident=SMSProviderIdent.alidayu, weight=0)
