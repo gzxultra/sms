7c04fb9f84252b7b59fee51ad21cb1814f3f4832
cadl <ctrlaltdeleteliu@gmail.com>
2017-02-23 14:09:44 +0800

    删除多余的配置项

--- conf/sample_default.py
+++ conf/sample_default.py
@@ -2,9 +2,7 @@
 
 
 class DefaultConfig(object):
-    THREAD_SAFE = False
     DEBUG = False
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
     DOMAIN = ''
 
     # 数据库配置
@@ -15,13 +13,6 @@ class DefaultConfig(object):
     DB_NAME = ''
     DB_POOL_MAX_CONNECTIONS = 60
     DB_POOL_STALE_TIMEOUT = 300  # sec
-    COMMIT_SELECT = True
-    FAKE_MEMCACHED = False
-    ENABLE_LOCAL_CACHED = True
-    MEMCACHED_ADDR = (
-        ('host:port', 'host:port'),
-        ('host:port', 'host:port'),
-    )
 
     BGTASK_MAX_WORKERS = 20
 
